da7d9e38a4da10364936faff0d0bffe085396e61
Abhinav Vsk Kompella <es21btech11002@iith.ac.in>
2025-03-26 11:59:38 +0530

    added views.py and models for recruiter

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recruiter',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- recruiter/models.py
+++ recruiter/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from jobListings.models import Industries
 
 # Create your models here.
 
+class RecruitingCompany(models.Model):
+    # company_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=100)
+    ndustry_id = models.ForeignKey(Industries, on_delete=models.CASCADE)
+    # company_logo = models.ImageField(upload_to='company_logos/')
+    company_description = models.TextField()
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.company_name
